7d15e8903ecc5ed89a611ddd996ada8ca9aeabde
Botasky <291770865@qq.com>
2018-10-26 11:28:33 +0800

    add:one item detail

--- lib/ui/one/One.dart
+++ lib/ui/one/One.dart
@@ -6,20 +6,59 @@ class OnePage extends StatefulWidget {
 }
 
 class _OnePageState extends State<OnePage> {
-  String _string = 'Test';
-
   @override
   void initState() {
     print("initState");
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('One is all'),
+        body: Center(
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return OneDetailItem();
+        },
+        itemCount: 20,
+      ),
+    ));
+  }
+}
+
+class OneDetailItem extends StatefulWidget {
+  @override
+  _OneDetailItemState createState() => _OneDetailItemState();
+}
+
+class _OneDetailItemState extends State<OneDetailItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Stack(
+      children: <Widget>[
+//        Image.network(
+//          'https://raw.githubusercontent.com/flutter/website/master/src/_includes/code/layout/lakes/images/lake.jpg',
+//          fit: BoxFit.cover,
+//          height: 240.0,
+//          width: double.infinity,
+//        ),
+        FadeInImage.assetNetwork(
+            width: double.infinity,
+            fit: BoxFit.cover,
+            height: 240.0,
+            placeholder: 'assets/images/ic_launcher_foreground.png',
+            image:
+                'https://github.com/flutter/website/blob/master/src/_includes/code/layout/lakes/images/lake.jpg?raw=true'),
+        Container(
+            margin: EdgeInsets.all(20),
+            child: Text(
+                'SizedBox.expand will make the button take full width and height, which is not the question about. The question is about a button covering full width only not height',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.blue[800],
+                    fontStyle: FontStyle.normal))),
+      ],
     ));
   }
 }
