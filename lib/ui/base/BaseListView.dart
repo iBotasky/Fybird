397e9b4f7ec9c7339b0b8557f8d1376783f93f83
Botasky <291770865@qq.com>
2018-12-24 16:35:42 +0800

    update for annoations

---
+++ lib/ui/base/BaseListView.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class BaseListWidget extends StatefulWidget {
+  @override
+  _BaseListWidgetState createState() => _BaseListWidgetState();
+}
+
+class _BaseListWidgetState extends State<BaseListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/uitl/NavigatorUtils.dart
+++ lib/uitl/NavigatorUtils.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cybird/ui/movie/MovieDetail.dart';
 
 class NavigatorUtils {
+  ///跳转到电影详情页面
   static toMovieDetail(BuildContext context, String title, String id) {
     Navigator.push(
         context,
